9f8e71f51e206919f2fb3f6cde210fceb6ac71b1
Bodake, Ayanna Ramesh <abodake3@gatech.edu>
2026-06-12 20:39:23 -0400

    randomized tests and performance

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ Will verify simulation using Questa Intel FPGA Starter Edition.
 
 In progress
 
+## Performance (simulation)
+
+
+Pipeline stages: 2 
+Latency:2 clock cycles
+Throughput: 1 matrix-vector multiplication per clock cycle
+Data width: INT8 weights, INT32 accumulator 
+Matrix size: 4×4
+
 ## Tools
 - SystemVerilog (IEEE 1800-2012)
 - Questa Intel FPGA Starter Edition
--- scripts/python_ref.py
+++ scripts/python_ref.py
@@ -1,10 +1,25 @@
 import numpy as np
+import random
+
 
 def mat_vec_mul(matrix, vector):
     matrix = np.array(matrix, dtype=np.int8) #8 bit matrix
     vector = np.array(vector, dtype=np.int8) #8 bit vector
     return matrix.astype(np.int32) @ vector.astype(np.int32) #dot product
 
+
+def run_random_tests(n=10):
+    for i in range(n):
+        matrix = np.random.randint(-128, 128, size=(4, 4), dtype=np.int8)
+        vector = np.random.randint(-128, 128, size=4, dtype=np.int8)
+        result = mat_vec_mul(matrix, vector)
+        print(f"Test {i+1}: matrix={matrix} vector={vector} result={result.tolist()}")
+
+run_random_tests(10)
+
+
+
+"""
 #tests
 #identity matrix
 matrix1 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
@@ -20,3 +35,5 @@ print("test 2: ", mat_vec_mul(matrix1, vector1))  # [4,4,4,4]
 matrix1 = [[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1],[-1,-1,-1,-1]]
 vector1 = [1,1,1,1]
 print("test 3: ", mat_vec_mul(matrix1, vector1))  # [-4,-4,-4,-4]
+"""
+
